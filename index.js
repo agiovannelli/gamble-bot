955070bdac284fd49ecbc036ee51a9783505fb06
Alex Giovannelli <agiovannelli2234@gmail.com>
2021-01-15 15:23:25 -0500

    Add logic to determine numeric hand value, add function descriptions

--- index.js
+++ index.js
@@ -13,3 +13,6 @@ console.log('Player hit!');
 blackjackManager.Hit(players[0]);
 console.log('Player seat: 0');
 console.log(players[0].hand);
+
+blackjackManager.DetermineHandValue(players[0]);
+console.log(players[0].handValue);
--- src/blackjackManager.js
+++ src/blackjackManager.js
@@ -3,13 +3,72 @@
 const deckManager = require('./deckManager.js');
 let deck;
 
+/**
+ * Creates a base player object.
+ * @function createPlayer
+ * @param {position value of player at table (left->right)} playerSeatNum 
+ * @private
+ */
 function createPlayer(playerSeatNum) {
     return {
         seat: playerSeatNum,
-        hand: []
+        hand: [],
+        handValue: 0
     }
 }
 
+/**
+ * Determines whether to provide max or min ace value for player hand.
+ * @function handleAcesInHand
+ * @param {player object} player 
+ * @param {array of aces} aces
+ * @private 
+ */
+function handleAcesInHand(player, aces) {
+    aces.forEach(ace => {
+        if(player.handValue + 11 > 21) {
+            player.handValue += 1;
+        } else {
+            player.handValue += 11;
+        }
+    });
+}
+
+/**
+ * Determines the player's numeric hand value from hand property.
+ * @function DetermineHandValue
+ * @param {player object} player 
+ * @public
+ */
+function DetermineHandValue(player) {
+    player.handValue = 0;
+    let aces = [];
+    player.hand.forEach(card => {
+        switch (card.value) {
+            case 'Ace':
+                aces.push(card);
+                break;
+            case 'King':
+            case 'Queen':
+            case 'Jack':
+                player.handValue += 10;
+                break;
+            default:
+                player.handValue += parseInt(card.value);
+                break;
+        }
+    });
+    if(aces.length) {
+        handleAcesInHand(player, aces);
+    }
+}
+
+/**
+ * Creates an array of player objects from provided number of desired players.
+ * @function RegisterPlayers
+ * @param {int for total number of player objects to create} numOfPlayers 
+ * @public
+ */
 function RegisterPlayers(numOfPlayers) {
     var players = [];
     for(let i = 0; i < numOfPlayers; i++) {
@@ -19,6 +78,12 @@ function RegisterPlayers(numOfPlayers) {
     return players;
 }
 
+/**
+ * Creates new deck and shuffles, deals cards to players at table.
+ * @function NewGame
+ * @param {array of players to initial deal hands} players 
+ * @public
+ */
 function NewGame(players) {
     deck = deckManager.createDeck();
     deck = deckManager.shuffleDeck(deck);
@@ -30,6 +95,12 @@ function NewGame(players) {
     }
 }
 
+/**
+ * Deals a card to the player provided.
+ * @function Hit
+ * @param {player object} player 
+ * @public
+ */
 function Hit(player) {
     if(deck.length) {
         player.hand.push(deck.pop());
@@ -39,5 +110,6 @@ function Hit(player) {
 module.exports = {
     RegisterPlayers: RegisterPlayers,
     NewGame: NewGame,
-    Hit: Hit
+    Hit: Hit,
+    DetermineHandValue: DetermineHandValue
 }
