93e8e26a5051ee1f8143cd6178700a68bc2257ac
Alex Giovannelli <agiovannelli2234@gmail.com>
2021-01-17 16:53:03 -0500

    Add function block comments

--- src/chatBot.js
+++ src/chatBot.js
@@ -5,12 +5,25 @@ const { Message } = require("discord.js");
 let currentChannel;
 let botId;
 
+/**
+ * Performs initial configurations for chat bot.
+ * @function SetupChatBot
+ * @param {Discord API client object} client 
+ * @param {dotenv API properties} envProps 
+ * @public
+ */
 function SetupChatBot(client, envProps) {
     botId = envProps.BOT_ID;
     currentChannel = client.channels.cache.get(envProps.CHANNEL_ID);
     currentChannel.send('Blackjack-chan is now online!');
 }
 
+/**
+ * Callback function to handle Discord messages from users.
+ * @function MessageHandler
+ * @param {Discord API message object} msg 
+ * @public
+ */
 function MessageHandler(msg) {
     if(msg.author.id !== botId) {
         switch(msg.content.toLowerCase()){
