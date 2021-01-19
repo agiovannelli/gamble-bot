cfe92794e9c9f51a24284c99b395edc68857f7ba
Alex Giovannelli <agiovannelli2234@gmail.com>
2021-01-18 21:32:46 -0500

    Add player collection functionality

--- src/chatBot.js
+++ src/chatBot.js
@@ -1,9 +1,44 @@
 'use strict';
 
-const { Message } = require("discord.js");
+const Discord = require("discord.js");
 
 let currentChannel;
+let messageHandlerLocked;
 let botId;
+let playerMap;
+
+/**
+ * Collects unique players from chat who requested to join via Discord.MessageCollector.
+ * @function collectPlayers
+ * @private
+ */
+function collectPlayers() {
+    messageHandlerLocked = true;
+    playerMap = new Map();
+
+    currentChannel.send('Starting a game of blackjack! Type \'join\' to claim your seat at the table!')
+
+    let collectorOptions = {
+        time: 10000
+    };
+    let collectorFilter = msg => msg.content.toLowerCase() === 'join';
+    let playerCollector = new Discord.MessageCollector(currentChannel, collectorFilter, collectorOptions);
+
+    playerCollector.on('collect', m => {
+        if(!playerMap.get(m.author.id)) {
+            m.reply('Welcome to the table!');
+            playerMap.set(m.author.id, {name: m.author.username});
+        }
+    });
+
+    playerCollector.on('end', () => {
+        playerMap.forEach((value, key) => {
+            console.log('Player registered: ' + value.name)
+        });
+        currentChannel.send('Player collection complete!');
+        messageHandlerLocked = false;
+    });
+}
 
 /**
  * Performs initial configurations for chat bot.
@@ -25,10 +60,10 @@ function SetupChatBot(client, envProps) {
  * @public
  */
 function MessageHandler(msg) {
-    if(msg.author.id !== botId) {
+    if(msg.author.id !== botId && !messageHandlerLocked) {
         switch(msg.content.toLowerCase()){
-            case 'test':
-                msg.reply('echo two bravo');
+            case 'blackjack':
+                collectPlayers();
                 break;
             default:
                 msg.reply('listening!');
