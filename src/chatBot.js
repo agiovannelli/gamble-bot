f742dfcf39e13b3981339c2131f110bdb97b2de4
Alex Giovannelli <agiovannelli2234@gmail.com>
2021-01-20 22:07:46 -0500

    Update table setup logic, provide table member display logic, add help options

--- src/chatBot.js
+++ src/chatBot.js
@@ -8,6 +8,44 @@ let botId;
 let playerMap;
 
 /**
+ * Notifies channel when an input is entered in chat but not recognized.
+ * @param {Message received in MessageHandler} msg 
+ * @function defaultMessage
+ * @private
+ */
+function defaultMessage(msg) {
+    msg.reply('Oops! I didn\'t quite get that... Please be patient with me and try again ~~ UwU');
+    currentChannel.send('Try typing \'help\' for a list of commands!');
+}
+
+/**
+ * Sends MessageHandler case options to channel.
+ * @function displayHelpOptions
+ * @private
+ */
+function displayHelpOptions() {
+    currentChannel.send('It looks like you need reminded of all that I can do ~~ ^_^');
+    currentChannel.send('To gather players for a game at the game table, type \'setup\'.');
+    currentChannel.send('To list the current players at the game table, type \'table\'.');
+}
+
+/**
+ * Iterates through playerMap and sends a message in channel regarding player name and seat position at table.
+ * @function listPlayersAtTable
+ * @private
+ */
+function listPlayersAtTable() {
+    currentChannel.send('Let\'s see who\'s at the table!');
+    if(playerMap && Array.from(playerMap.entries()).length) {
+        playerMap.forEach((value) => {
+            currentChannel.send(`Seat ${value.seat + 1}: ${value.name}`);
+        });
+    } else {
+        currentChannel.send('I\'m here all by myself... >_<');
+    }
+}
+
+/**
  * Collects unique players from chat who requested to join via Discord.MessageCollector.
  * @function collectPlayers
  * @private
@@ -16,8 +54,9 @@ function collectPlayers() {
     messageHandlerLocked = true;
     playerMap = new Map();
 
-    currentChannel.send('Starting a game of blackjack! Type \'join\' to claim your seat at the table!')
+    currentChannel.send('Seats are available at the game table! Type \'join\' to claim your seat! Only 4 spots are available!');
 
+    let seatCounter = 0;
     let collectorOptions = {
         time: 10000
     };
@@ -25,17 +64,16 @@ function collectPlayers() {
     let playerCollector = new Discord.MessageCollector(currentChannel, collectorFilter, collectorOptions);
 
     playerCollector.on('collect', m => {
-        if(!playerMap.get(m.author.id)) {
-            m.reply('Welcome to the table!');
-            playerMap.set(m.author.id, {name: m.author.username});
+        if(!playerMap.get(m.author.id) && seatCounter < 4) {
+            m.reply('welcome to the table!');
+            playerMap.set(m.author.id, {name: m.author.username, seat: seatCounter});
+            seatCounter++;
         }
     });
 
     playerCollector.on('end', () => {
-        playerMap.forEach((value, key) => {
-            console.log('Player registered: ' + value.name)
-        });
-        currentChannel.send('Player collection complete!');
+        currentChannel.send('Seats are now reserved!');
+        listPlayersAtTable();
         messageHandlerLocked = false;
     });
 }
@@ -62,11 +100,17 @@ function SetupChatBot(client, envProps) {
 function MessageHandler(msg) {
     if(msg.author.id !== botId && !messageHandlerLocked) {
         switch(msg.content.toLowerCase()){
-            case 'blackjack':
+            case 'setup':
                 collectPlayers();
                 break;
+            case 'table':
+                listPlayersAtTable();
+                break;
+            case 'help':
+                displayHelpOptions();
+                break;
             default:
-                msg.reply('listening!');
+                defaultMessage(msg);
                 break;
         }
     }
